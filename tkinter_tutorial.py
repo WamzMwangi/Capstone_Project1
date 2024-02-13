feccf41d627a2cfd1c004710035e9efd910ddca2
“Wambui” <mwangi25.mercy@gmail.com>
2024-02-13 20:35:54 +0300

    Testing the functionality of buttons

--- tkinter_tutorial.py
+++ tkinter_tutorial.py
@@ -1,16 +1,37 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
+#create a dictionary for each topic, each dict is a key-value pair with a list
+Variables_dict={
+    'How can you convert a variable from one data type to another in python?': ['switch(variable_name)','change(variable_name)','type(variable_name)','convert(variable_name)'],
+    'What is a variable?':['A constant value' , 'A reserved keyword in Python','A placeholder for storing data' ,' A mathematical operation']
+}
+Iterations_dict={
+    'What is the difference between for and while loop?': ['for loops are used for indefinite iteration and while loops for definite iteration', 
+                'There is no difference between them', 
+                'while loops are used for indefinite iteration and for loops are for definite iteration',
+                'while" loops can only iterate over numbers, for loops can iterate over any data type'],
+    'How do you exit a loop prematurely in python?':['exit statement', 'stop statement','break statement','range statement']
+}
+#create functions that display the questions after topic is chosen
+
+def variable_topic_chosen():
+    for questions in Variables_dict.keys():
+        q_label=tk.Label(window, text=questions)
+        q_label.pack
+
+def iterations_topic_chosen():
+    for questions in Iterations_dict.keys():
+        q_label=tk.Label(window, text=questions)
+        q_label.pack
+
+
+#create the basic layout of the GUI
 window=tk.Tk()
 
 #let us create the first window that allows user to choose a topic to study
 window.geometry('800x500')
 window.title('Studdy Buddy Quiz App')
-
-def return_pressed(event):
-    print('Return key pressed.')
-
-def log(event):
-    print(event)
 
 label=tk.Label(window, text='Test your knowledge of Python with StudyBuddy!!!', font=('Times New Roman',24), fg='blue')
 label.pack(padx=10,pady=10)
@@ -18,9 +39,8 @@ label.pack(padx=10,pady=10)
 label=tk.Label(window,text='Please choose a topic you want to study', font=('Times New Roamn',18))
 label.pack()
 
-topic1=tk.Button(window,text='VARIABLES', font=('Gothic',14),fg='green')
+topic1=tk.Button(window,text='VARIABLES', font=('Gothic',14),fg='green',command=variable_topic_chosen)
 topic1.pack()
-topic1.bind('<Return>', return_pressed)
 
 topic2=tk.Button(window,text='ITERATIONS', font=('Gothic',14),fg='green')
 topic2.pack()
