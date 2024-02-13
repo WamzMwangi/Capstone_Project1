8f1cf7194f7b08c2c761dda18e3ee2d6140bb785
“Wambui” <mwangi25.mercy@gmail.com>
2024-02-13 19:37:14 +0300

    Tidying up

--- tkinter_tutorial.py
+++ tkinter_tutorial.py
@@ -6,6 +6,11 @@ window=tk.Tk()
 window.geometry('800x500')
 window.title('Studdy Buddy Quiz App')
 
+def return_pressed(event):
+    print('Return key pressed.')
+
+def log(event):
+    print(event)
 
 label=tk.Label(window, text='Test your knowledge of Python with StudyBuddy!!!', font=('Times New Roman',24), fg='blue')
 label.pack(padx=10,pady=10)
@@ -13,11 +18,12 @@ label.pack(padx=10,pady=10)
 label=tk.Label(window,text='Please choose a topic you want to study', font=('Times New Roamn',18))
 label.pack()
 
-button=tk.Button(window,text='VARIABLES', font=('Gothic',14),fg='green')
-button.pack()
+topic1=tk.Button(window,text='VARIABLES', font=('Gothic',14),fg='green')
+topic1.pack()
+topic1.bind('<Return>', return_pressed)
 
-button=tk.Button(window,text='ITERATIONS', font=('Gothic',14),fg='green')
-button.pack()
+topic2=tk.Button(window,text='ITERATIONS', font=('Gothic',14),fg='green')
+topic2.pack()
 
 window.mainloop()
 
