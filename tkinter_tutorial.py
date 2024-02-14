ff0f922a9c99ce435568f3b2793a9a423ba7a6d1
“Wambui” <mwangi25.mercy@gmail.com>
2024-02-14 23:16:43 +0300

    trying to check for correct answer

--- tkinter_tutorial.py
+++ tkinter_tutorial.py
@@ -2,10 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 #create a dictionary for each topic, each dict is a key-value pair with a list
-Variables_dict={
-    'How can you convert a variable from one data type to another in python?': ['switch(variable_name)','change(variable_name)','type(variable_name)','convert(variable_name)'],
-    'What is a variable?':['A constant value' , 'A reserved keyword in Python','A placeholder for storing data' ,' A mathematical operation']
-}
+
 Iterations_dict={
     'What is the difference between for and while loop?': ['for loops are used for indefinite iteration and while loops for definite iteration', 
                 'There is no difference between them', 
@@ -15,29 +12,37 @@ Iterations_dict={
 }
 #counter for questions
 current_q_index=0
+#counter for score
+score_count=0
+
+#now a function that checks for the results
+def check_answer():
+    global current_q_index, score_count
+    selected_answer=user_answer.get()
+    correct_answer=Iterations_dict[list(Iterations_dict.keys())[current_q_index]][2]
+    if correct_answer==selected_answer:
+        labelA=tk.Label(window,text='CORRECT!', font=('Helvetica',20),fg='blue')
+        labelA.pack()
 
 #Now to display each question at a time
 def display_quiz():
     for widget in window.winfo_children():
         widget.destroy()
-    global current_q_index
+    global current_q_index,user_answer,sorted
     if current_q_index<len(Iterations_dict):
         questions, choices=list(Iterations_dict.items())[current_q_index]
         q_label=tk.Label(window, text=questions)
         q_label.pack()
         
         for x in choices:
-            choices_button=ttk.Radiobutton(window, text=x, variable=student_answer,value=x)
+            choices_button=ttk.Radiobutton(window, text=x, variable=user_answer,value=x)
             choices_button.pack()
         current_q_index=current_q_index+1
         Next_button=tk.Button(window, text ='Next', command= display_quiz)
         Next_button.pack()
     else:
          messagebox.showinfo('Well Done!!','This is the end of the QUIZ')
-
-#Now we include a function to take user to next question
-#def next_q(current_index):
-
+  
 
 #create the basic layout of the GUI
 window=tk.Tk()
@@ -46,15 +51,12 @@ window=tk.Tk()
 window.geometry('800x500')
 window.title('Studdy Buddy Quiz App')
 
-student_answer=tk.StringVar()
+user_answer=tk.StringVar()
 label=tk.Label(window, text='Test your knowledge of Python with StudyBuddy!!!', font=('Times New Roman',24), fg='blue')
 label.pack(padx=10,pady=10)
 
 label=tk.Label(window,text='Please choose a topic you want to study', font=('Times New Roamn',18))
 label.pack()
-
-#topic1=tk.Button(window,text='VARIABLES', font=('Gothic',14),fg='green',command=variable_topic_chosen)
-#topic1.pack()
 
 topic2=tk.Button(window,text='ITERATIONS', font=('Gothic',14),fg='green',command=display_quiz)
 topic2.pack()
